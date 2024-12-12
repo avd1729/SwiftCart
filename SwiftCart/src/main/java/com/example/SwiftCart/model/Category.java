1514de3fd3759f9c367a6a658c2e42bccc1d7c6a
Aravind <aravind19112003@gmail.com>
2024-12-12 15:50:19 +0530

    Add data models

---
+++ SwiftCart/src/main/java/com/example/SwiftCart/model/Category.java
@@ -0,0 +1,11 @@
+package com.example.SwiftCart.model;
+
+import java.util.List;
+
+public class Category {
+    private Long id;
+    private String name;
+
+    private List<Product> products;
+
+}
