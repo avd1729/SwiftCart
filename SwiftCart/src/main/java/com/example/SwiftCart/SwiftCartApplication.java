c7aa688836a007c1841819f2d55483eab025236d
Aravind <aravind19112003@gmail.com>
2024-12-12 15:19:00 +0530

    first commit

---
+++ SwiftCart/src/main/java/com/example/SwiftCart/SwiftCartApplication.java
@@ -0,0 +1,13 @@
+package com.example.SwiftCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftCartApplication.class, args);
+	}
+
+}
---
+++ SwiftCart/src/test/java/com/example/SwiftCart/SwiftCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SwiftCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
