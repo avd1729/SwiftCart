99652d5096e9fdaa8b598c7d13341d178fa6e9d5
Aravind <aravind19112003@gmail.com>
2024-12-12 20:43:48 +0530

    Add Category service

---
+++ SwiftCart/src/main/java/com/example/SwiftCart/service/category/CategoryService.java
@@ -0,0 +1,59 @@
+package com.example.SwiftCart.service.category;
+
+import com.example.SwiftCart.exceptions.AlreadyExistsException;
+import com.example.SwiftCart.model.Category;
+import com.example.SwiftCart.repository.CategoryRepository;
+import com.example.SwiftCart.exceptions.CategoryNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService implements ICategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public Category getCategoryById(Long id) {
+         return categoryRepository.findById(id)
+                 .orElseThrow(() -> new CategoryNotFoundException("Category not found!"));
+    }
+
+    @Override
+    public Category getCategoryByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category addCategory(Category category) {
+        return Optional.of(category).filter(c -> !categoryRepository.existsByName(c.getName()))
+                .map(categoryRepository::save)
+                .orElseThrow(() -> new AlreadyExistsException("Category already exists!"));
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long id) {
+        return Optional.ofNullable(getCategoryById(id))
+                .map(oldCategory -> {
+                    oldCategory.setName(category.getName());
+                    return categoryRepository.save(oldCategory);
+                })
+                .orElseThrow(() -> new CategoryNotFoundException("Category not found!"));
+    }
+
+    @Override
+    public void deleteCategoryById(Long id) {
+        categoryRepository.findById(id)
+                .ifPresentOrElse(categoryRepository::delete, () -> {
+                    throw new CategoryNotFoundException("Category not found!");
+                });
+    }
+}
