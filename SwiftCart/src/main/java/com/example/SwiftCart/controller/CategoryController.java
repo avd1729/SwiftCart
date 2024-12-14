49cd2a749e2abfcd461dc97e4b3ca1dcef359655
Aravind <aravind19112003@gmail.com>
2024-12-14 15:36:03 +0530

    Add Category controller

--- SwiftCart/src/main/java/com/example/SwiftCart/controller/CategoryController.java
+++ SwiftCart/src/main/java/com/example/SwiftCart/controller/CategoryController.java
@@ -1,4 +1,88 @@
 package com.example.SwiftCart.controller;
 
+import com.example.SwiftCart.model.Category;
+import com.example.SwiftCart.response.ApiResponse;
+import com.example.SwiftCart.service.category.ICategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("${api.prefix}/categories")
+@RequiredArgsConstructor
 public class CategoryController {
+
+    private final ICategoryService categoryService;
+
+    @GetMapping("/category/all")
+    public ResponseEntity<ApiResponse> getAllCategories() {
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Found!", categories));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse("Error: ", e.getMessage()));
+        }
+    }
+
+    @PostMapping("/category/add")
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody Category name) {
+        try {
+            Category category = categoryService.addCategory(name);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse("Added category!", category));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(new ApiResponse("Error: ", e.getMessage()));
+        }
+    }
+
+    @GetMapping("/{id}/get")
+    public ResponseEntity<ApiResponse> getCategoryById(@PathVariable Long id) {
+        try {
+            Category category = categoryService.getCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Found!", category));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ApiResponse("Error: ", e.getMessage()));
+        }
+
+    }
+
+    @GetMapping("/category/{name}/get")
+    public ResponseEntity<ApiResponse> getCategoryByName(@PathVariable String name) {
+        try {
+            Category category = categoryService.getCategoryByName(name);
+            return ResponseEntity.ok(new ApiResponse("Found!", category));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ApiResponse("Error: ", e.getMessage()));
+        }
+    }
+
+    @DeleteMapping("/category/{id}/delete")
+    public ResponseEntity<ApiResponse> deleteCategoryById(@PathVariable Long id) {
+        try {
+            Category category = categoryService.getCategoryById(id);
+            categoryService.deleteCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Deleted category!", category));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ApiResponse("Error: ", e.getMessage()));
+        }
+    }
+
+    @PutMapping("/category/{id}/update")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        try {
+            Category updatedCategory = categoryService.updateCategory(category, id);
+            return ResponseEntity.ok(new ApiResponse("Updated category!", updatedCategory));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ApiResponse("Error: ", e.getMessage()));
+        }
+    }
+
 }
--- SwiftCart/src/main/java/com/example/SwiftCart/controller/ImageController.java
+++ SwiftCart/src/main/java/com/example/SwiftCart/controller/ImageController.java
@@ -25,7 +25,7 @@ public class ImageController {
 
     private final ImageService imageService;
 
-    @PostMapping("/upload")
+    @PostMapping("image/upload")
     public ResponseEntity<ApiResponse> saveImages(@RequestParam List<MultipartFile> files, @RequestParam Long productId) {
         try {
             List<ImageDto> imageDtos = imageService.saveImages(files, productId);
